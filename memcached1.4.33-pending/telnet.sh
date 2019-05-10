bad7beea08c903a4631a52d88c3729f6793f992b
VictorYYW <slark@umich.edu>
2019-05-10 18:25:06 -0400

    add two bugs needing spymemcache as client

--- memcached1.4.33-pending/telnet.sh
+++ memcached1.4.33-pending/telnet.sh
@@ -1,38 +1,12 @@
 #!/usr/bin/expect
 
 spawn telnet 127.0.0.1 11211
-for {set NUM 0} {$NUM < 9999} {incr NUM} {
-  if { $NUM / 10 == 0 } {
-    send "set key$NUM 0 0 1\r"
-    send "$NUM\r"
-    expect "STORED"
-    continue
-  }
-  if { $NUM / 100 == 0 } {
-    send "set key$NUM 0 0 2\r"
-    send "$NUM\r"
-    expect "STORED"
-    continue
-  }
-  if { $NUM / 1000 == 0 } {
-    send "set key$NUM 0 0 3\r"
-    send "$NUM\r"
-    expect "STORED"
-    continue
-  }
-  if { $NUM / 10000 == 0 } {
-    send "set key$NUM 0 0 4\r"
-    send "$NUM\r"
-    expect "STORED"
-    continue
-  }
-  if { $NUM / 100000 == 0 } {
-    send "set key$NUM 0 0 5\r"
-    send "$NUM\r"
-    expect "STORED"
-    continue
-  }
+for {set NUM 0} {$NUM < 1000000} {incr NUM} {
+	send "set key$NUM 0 0 0\r"
+	send "\r"
+	expect "STORED"
 }
 send "lru_crawler metadump all\r"
+interact
 send "quit\r"
 close
