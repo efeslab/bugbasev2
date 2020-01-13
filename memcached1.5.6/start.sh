958e3baef05a93c0528fe177bea967976e0284a9
Ruiyang Zhu <ryanzhu@umich.edu>
2020-01-12 23:33:08 -0500

    fix everything for memcached1.5.6

--- memcached1.5.6/start.sh
+++ memcached1.5.6/start.sh
@@ -1,2 +1,2 @@
-memcached -S -v -u root &
-java -jar ReproduceMemcachedSegfault-1.0-SNAPSHOT-shaded.jar sammy test localhost 11211
+java -jar ReproduceMemcachedSegfault-1.0-SNAPSHOT-shaded.jar sammy test localhost 11211 > stdout.txt 2 > stderr.txt &
+memcached -S -v -u root
