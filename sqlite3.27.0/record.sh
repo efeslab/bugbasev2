6ea080177b6658c5e915bd572db40456fad96674
Alkaid <zgf574564920@gmail.com>
2020-05-08 07:35:47 -0400

    add scripts and recording config for sqlite3.27.0(sqlite-4e8e485)

--- sqlite3.27.0/record.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-klee -env-file=env -use-forked-solver=false -output-source=false -write-kqueries -write-paths --libc=uclibc --posix-runtime -pathrec-entry-point=__klee_posix_wrapped_main -ignore-posix-path=true -allocate-determ ./sqlite3.bc < test.sqlite
