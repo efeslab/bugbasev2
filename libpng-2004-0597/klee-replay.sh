7bc218a2d438c66beaf0d9b4f1b50313754400b2
Alkaid <zgf574564920@gmail.com>
2020-09-27 02:31:54 -0400

    Add klee record/replay related scripts to libpng bug

--- libpng-2004-0597/klee-replay.sh
+++ libpng-2004-0597/klee-replay.sh
@@ -1,1 +1,20 @@
-klee -solver-backend=stp -oracle-KTest=oracle.ktest -call-solver=false -use-forked-solver=false -output-source=false -write-kqueries -write-paths --libc=uclibc --posix-runtime -env-file=env -pathrec-entry-point="__klee_posix_wrapped_main" -ignore-posix-path=true -replay-path=klee-out-0/test000001.path -use-independent-solver=false -oob-check=true -allocate-determ ./poc.bc pngtest_bad.png -sym-file pngtest_bad.png 8574
+set -e
+if [ -z $1 ]; then
+  echo "${0}" '${ITER}'
+  exit -1
+fi
+ITER=$1
+source $(dirname $0)/klee-env.sh
+
+rm -rf ${KLEE_REPLAY_OUT_DIR}
+cp ${PREPASS_BC} ${RUN_BC}
+klee -solver-backend=stp -call-solver=false -output-stats=false \
+  -output-istats=false -oracle-KTest=oracle.ktest -use-forked-solver=false \
+  -output-source=false -write-kqueries -write-paths --libc=uclibc \
+  --posix-runtime -env-file=env \
+  -pathrec-entry-point="__klee_posix_wrapped_main" -ignore-posix-path=true \
+  -replay-path=${KLEE_RECORD_OUT_DIR}/test000001.path \
+  -use-independent-solver=false -oob-check=true -allocate-determ \
+  -all-external-warnings -output-dir=${KLEE_REPLAY_OUT_DIR} \
+  ${RUN_BC} pngtest_bad.png -sym-file pngtest_bad.png
+cp ${KLEE_RECORD_OUT_DIR}/${FREQ_BC} ${KLEE_REPLAY_OUT_DIR}
